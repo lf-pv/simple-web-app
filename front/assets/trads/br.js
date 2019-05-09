bb7d224ab1ed87fc4b57834b889d0d6f3903bb77
Louis Fortin <lfortin@adneom.com>
2019-05-09 17:42:59 +0200

    feat(front): add language

---
+++ front/assets/trads/br.js
@@ -0,0 +1,11 @@
+const getValues = () => {
+	return {
+		title: "Blurp",
+		text: "J'ai soiiifffff",
+		buttonText: "C'est quoi le bouton",
+		posted: "Mec",
+		version: "Chiffres",
+		subComponent: "..."
+	};
+};
+module.exports = getValues;
--- front/services/translate.service.js
+++ front/services/translate.service.js
@@ -1,5 +1,6 @@
 const en = require("../assets/trads/en");
 const fr = require("../assets/trads/fr");
+const br = require("../assets/trads/br");
 const cu = require("../assets/trads/custom");
 const _toolService = require("../services/tool.service");
 
@@ -7,11 +8,14 @@ const translate = (content, key, language) => {
 	let trads;
 	const defaultTrad = cu();
 	switch (language) {
-		case "en":
+		case 'en':
 			trads = en();
 			break;
-		case "fr":
+		case 'fr':
 			trads = fr();
+			break;
+		case 'br':
+			trads = br();
 			break;
 		default:
 			trads = cu();
@@ -27,7 +31,7 @@ const translate = (content, key, language) => {
 
 const translatePage = (page, language) => {
 	const regex = /[{]+\s[A-Za-z0-9]+\s[}]+/g;
-	const keys = page.match(regex).map(key => key.split(" ")[1]);
+	const keys = page.match(regex).map(key => key.split(' ')[1]);
 	keys.forEach(key => {
 		page = translate(page, key, language);
 	});
